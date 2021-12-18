cf5748122e82a5ddade7fa92526c22f4cde54462
tedymaradho <maradho@gmail.com>
2021-12-19 00:26:42 +0700

    download file image and save to local directory

--- Run.py
+++ Run.py
@@ -1,39 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+
 # import pandas as pd
 
-params = {
-    'addRecent': 'false',
-    'canChangeKeyword': 'false',
-    'includeSuggestions': 'false',
-    'searchId': 'r1yXPL'
-}
+res = requests.get('https://id.carousell.com/categories/mobile-phones-338/tablets-1241/?')
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                  ' Chrome/96.0.4664.93 Safari/537.36'
-
-}
-
-res = requests.get('https://id.carousell.com/categories/mobile-phones-338/tablets-1241/?', params=params,
-                   headers=headers)
+with open('result.html', 'w+', encoding='utf-8') as file:
+    file.write(res.text)
+    file.close()
 
 result = BeautifulSoup(res.text, 'html.parser')
-barangs = result.findAll('div', 'D_vf')
 
+barangs = result.findAll(attrs={'class': 'D_nt'})
+# print(result)
 file = open('hasil_scraping_carousell.csv', 'w', newline='')
 writer = csv.writer(file)
-headers = ['Nama Toko', 'Harga']
+headers = ['Nama barang', 'Harga']
 writer = writer.writerow(headers)
 # things = []
+
 for barang in barangs:
-    titles = barang.find(attrs={'data-testid': 'listing-card-text-seller-name'}).text
-    prices = barang.find(attrs={'class': 'D_fz M_eX D_eC M_cf D_f_ M_eY D_fC M_fb D_fF M_fe D_fI M_fh D_fL M_fk D_fv'}).text
+    titles = barang.find('img')['alt']
     try:
-        times = barang.find(attrs={'class': 'D_fz M_eX D_eA M_cc D_f_ M_eY D_fC M_fb D_fF M_fe D_fI M_fh D_fK M_fj D_vu M_xM D_fx'}).text
+        prices = barang.find('p', 'D_eS M_eV D_eA M_ci D_eT M_eW D_eW M_eZ D_eY M_fb D_fb M_ff D_fe M_fh D_eO').text
     except Exception:
-        times = 'Times not found'
+        prices = 'Not Found'
+    images = barang.find('img')['src']
 
     # data = {
     #     'title': titles,
@@ -42,11 +35,16 @@ for barang in barangs:
     # things.append(data)
     print(titles)
     print(prices)
-    print(times)
+    print(images)
+    # print(barang)
 
-# df = pd.DataFrame(things)
-# df.to_csv('hasil_scraping_carousell_pandas_indextrue.csv', index=True)
+    # df = pd.DataFrame(things)
+    # df.to_csv('hasil_scraping_carousell_pandas_indextrue.csv', index=True)
     file = open('hasil_scraping_carousell.csv', 'a', newline='', encoding='utf-8')
     writer = csv.writer(file)
     writer = writer.writerow([titles, prices])
     file.close()
+
+    with open('images/' + titles + '.jpg', 'wb') as f:
+        img = requests.get(images)
+        f.write(img.content)
